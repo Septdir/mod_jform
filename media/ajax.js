432f70f38a416f16275ed283ab84b07d5b6ad824
Septdir <septdir@gmail.com>
2018-03-06 15:46:30 +0300

    Happy new Year

--- media/ajax.js
+++ media/ajax.js
@@ -2,7 +2,7 @@
  * @package    JForm Module
  * @version    1.0.0
  * @author     Igor Berdicheskiy - septdir.ru
- * @copyright  Copyright (c) 2013 - 2017 Igor Berdicheskiy. All rights reserved.
+ * @copyright  Copyright (c) 2013 - 2018 Igor Berdicheskiy. All rights reserved.
  * @license    GNU/GPL license: http://www.gnu.org/copyleft/gpl.html
  * @link       https://septdir.ru
  */
